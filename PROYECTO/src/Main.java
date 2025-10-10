09709bfb7c06cc0ec84e2902f40fe73580699426
raulabadz <raulabad.z2308@gmail.com>
2025-10-10 11:40:24 +0200

    update

--- PROYECTO/src/Main.java
+++ PROYECTO/src/Main.java
@@ -1,2 +1,18 @@
 public class Main {
+
+    public static void main(String[]args){
+
+        System.out.println("Este es mi primer programa en Java");
+        System.out.println("Estas es la segunda linea del programa");
+
+    }
+
+    public void nombreMetodo(){
+
+
+
+    }
 }
+
+
+

e83361a76776902a0794fc4e63dd312bfb927691
raulabadz <raulabad.z2308@gmail.com>
2025-10-28 17:26:52 +0100

    commit

---
+++ MPO/MPO/src/entrada.java
@@ -0,0 +1,28 @@
+public class entrada {
+
+    public static void main(String[] args) {
+
+        System.out.println("Ejecución de metodos");
+        saludar("Borja");
+        saludar("Juana");
+        saludar("Pedro");
+        despedir();
+       // Operaciones.sumar(4,8);
+        // Operaciones.restar(10,5);
+        Operaciones operacionesmates = new Operaciones();
+                operacionesmates.sumar(5,9);
+
+
+
+    }
+    public static void saludar(String data){
+        System.out.println("Hola "+data);
+        System.out.println("Este metodo se va a repetir en el saludo");
+
+
+    }
+    public static void despedir(){
+        System.out.println("Aquí me despido");
+        System.out.println("Hasta luego");
+    }
+}
